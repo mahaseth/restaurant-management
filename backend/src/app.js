e3487d181f0e1bd0cf713fc658075fb2a0ac8019
manjish12 <chaulagainmanjish12@gmail.com>
2025-12-05 13:59:05 +0545

    Connected Mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { homeRouter } from "./routes/home.routes.js";
 
 dotenv.config();
 
+
 const app = express();
+mongoose.connect("mongodb+srv://manjishchaulagain80_db_user:ZnUUWVkEf0GeRZS9@cluster0.3jtwejj.mongodb.net/").then(() => {console.log("Connected to MongoDB")}).catch((err) => {console.error("Failed to connect to MongoDB", err)});    
+
 app.use(cors());
 app.use(express.json());
 app.use("/", homeRouter);
