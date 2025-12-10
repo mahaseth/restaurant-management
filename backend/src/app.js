124a897fd5a2ec61e2054d18ed08d9a91d6718d8
mahaseth <yogeshmahaseth07@gmail.com>
2025-12-10 19:40:58 +0545

    Added User model, route, controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import config from "./config/config.js";
 import { homeRouter } from "./routes/home.routes.js";
+import userRoute from "./routes/user.routes.js";
 import connectDB from "./config/database.js";
 
 
@@ -10,6 +11,7 @@ connectDB();
 app.use(bodyParser.json());
 
 app.use("/", homeRouter);
+app.use("/api/users", userRoute);
 
 app.listen(config.port, () => {
   console.log(`Server is running at port: ${config.port}...`);
